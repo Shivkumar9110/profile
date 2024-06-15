3b97b38af5e0219950d350d4adc485fe7abd24a9
Shiv kumar <boydabang27@gmail.com>
2024-06-15 20:39:13 +0530

    This is version 01.000.01

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let Changefont_size=setInterval(fun,3000)
+
+function fun(){
+    let mainsection_leftside_text = document.querySelector("#leftside");
+   
+    mainsection_leftside_text.style.fontStyle = "italic";
+}
+
+
+
